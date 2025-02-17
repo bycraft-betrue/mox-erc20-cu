57ee833b3003b423d1856b15a5e597e1ccd58a91
bycraft-betrue <bycraft.betrue@gmail.com>
2025-02-17 13:49:58 +0200

    revisit stateless test

--- contracts/sub_lesson/stateless_fuzz_solvable.vy
+++ contracts/sub_lesson/stateless_fuzz_solvable.vy
@@ -1,4 +1,4 @@
-# pragma version 0.4.0
+# pragma version ^0.4.0
 """
 @title always_return_input
 @license MIT
--- tests/sub_lesson/test_stateless.py
+++ tests/sub_lesson/test_stateless.py
@@ -3,10 +3,13 @@ from hypothesis import given, HealthCheck, settings
 from contracts.sub_lesson import stateless_fuzz_solvable
 
 
+# we added this bit to suppress the health check warning for fixture, but then commented it out
 @settings(
     max_examples=1000, suppress_health_check=[HealthCheck.function_scoped_fixture]
 )
-@given(input=strategy("uint256"))  # 0, MaxUINT256
+@given(
+    input=strategy("uint256")
+)  # 0, MaxUINT256: strategy helps to generate random inputs without infinity being max
 def test_always_returns_input_number(input):
     contract = stateless_fuzz_solvable.deploy()
     assert contract.always_returns_input_number(input) == input
